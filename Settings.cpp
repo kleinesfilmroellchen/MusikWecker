f0028de3d9bf425467f53d4930dedc6dacb510ba
kleines Filmröllchen <filmroellchen@serenityos.org>
2024-03-16 23:20:44 +0100

    remove unnecessary second memcmp
    this is already implemented in the HAL, so we're wasting a lot of time here

--- Settings.cpp
+++ Settings.cpp
@@ -3,18 +3,11 @@
 
 void save_settings()
 {
-	EepromSettings stored_settings;
-	EEPROM.get(SETTINGS_ADDRESS, stored_settings);
-	auto are_same = memcmp(&stored_settings, &eeprom_settings, SETTINGS_SIZE) == 0;
-	if (are_same) {
-		debug_print(F("Settings didn't change; not writing EEPROM."));
+	debug_print(F("Writing settings to EEPROM..."));
+	EEPROM.put(SETTINGS_ADDRESS, eeprom_settings);
+	if (EEPROM.commit()) {
+		debug_print(F(" Writing success."));
 	} else {
-		debug_print(F("Writing settings to EEPROM..."));
-		EEPROM.put(SETTINGS_ADDRESS, eeprom_settings);
-		if (EEPROM.commit()) {
-			debug_print(F(" Writing success."));
-		} else {
-			debug_print(F(" Writing FAILED."));
-		}
+		debug_print(F(" Writing FAILED."));
 	}
 }
